06744b489578eb8c7a83a62e274e80bdc3b8348a
Victor Stark <luan.victtor@hotmail.com>
2025-09-16 12:43:59 -0300

    Refatore o esquema SQL para incluir novas tabelas
    Por algum motivo, salvou metade do codigo, então fiz a correção.

--- DB Relacionais 4P/Aula_FK.sql
+++ DB Relacionais 4P/Aula_FK.sql
@@ -1,9 +1,16 @@
-CREATE TABLE ensalamento (
-	id_ensalamento SERIAL PRIMARY KEY,
-	curso_id INTEGER REFERENCES cursos_disciplinas(id),
-	sala_id INTEGER REFERENCES salas(id),
-	professor_id INTEGER REFERENCES professores(id),
-	data_hora TIMESTAMPTZ
+CREATE TABLE professores (
+	id_professor SERIAL PRIMARY KEY,
+	nome VARCHAR(100) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	departamento VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE alunos (
+	id_aluno SERIAL PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+	matricula VARCHAR(100) UNIQUE NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	curso_id INTEGER REFERENCES cursos_disciplinas(id_curso)
 );
 
 CREATE TABLE salas (
@@ -13,7 +20,20 @@ CREATE TABLE salas (
 	localizacao VARCHAR(100)
 );
 
-CREATE TABLE professores (
-	id_professor SERIAL PRIMARY KEY,
-	nome VARCHAR()
-)
+CREATE TABLE cursos_disciplinas (
+	id_curso SERIAL PRIMARY KEY,
+	nome_curso VARCHAR(100) NOT NULL,
+	codigo VARCHAR(100) UNIQUE NOT NULL,
+	disciplina VARCHAR(100) NOT NULL,
+	periodo VARCHAR(100) NOT NULL,
+	professor_id INTEGER REFERENCES professores(id_professor)
+);
+
+CREATE TABLE ensalamento (
+	id_ensalamento SERIAL PRIMARY KEY,
+	curso_id INTEGER REFERENCES cursos_disciplinas(id_curso),
+	sala_id INTEGER REFERENCES salas(id_sala),
+	professor_id INTEGER REFERENCES professores(id_professor),
+	data_hora TIMESTAMPTZ DEFAULT NOW()
+);
+
